ad4fd07383c40dd55a426880680dc1b5572a2546
Jatin-sharma01 <amobsidian001@gmail.com>
2025-05-01 05:33:04 +0530

    javascript variables
    tutorial no. 55 code with harry

--- Code With Harry ( HTML CSS JS )/Javascript/55_variables/script.js
+++ Code With Harry ( HTML CSS JS )/Javascript/55_variables/script.js
@@ -6,19 +6,19 @@ var c = "jatin";
 console.log( a + b + 9)
 console.log( typeof a, typeof b, typeof c)
 
-let x = "jatin";
-let y = 22;
-let z = 2.3;
-let t = true;
-let s = undefined;
-let q = null;
+let x = "jatin"; // its a string
+let y = 22; // number
+let z = 2.3; //number
+let t = true; //boolean
+let s = undefined; //undefined
+let q = null; //object, null is a type of object, dont forget it
 
 console.log( x, y, z, t, s, q)
 console.log( typeof x, typeof y, typeof z, typeof t, typeof s, typeof q)
 
 let o = {
     name: "jatin",
-    "job role": "Developer",
+    "job role": "Developer", // as there is space between job role so u have to write it in double quotes
     "is experienced": true,
     "Previous salary": 20000,
 }
@@ -32,7 +32,7 @@ console.log(o)
 // 1. letters, digits, underscore and dollar sign are allowed
 // 2. must begin with a dollar sign, _ or a letter
 // 3. javascript reserved words cant be used as variables
-// 4. Jatin and jatin are different variable
+// 4. Jatin and jatin are different variable ( basically javascript variables are case sensitive)
 
 //  var vs let in Js
 // 1. var is globally scoped while let and const are blocked scoped
